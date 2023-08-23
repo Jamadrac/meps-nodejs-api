dba2f1c504c0d376a261243739b017bf6208ae6c
jamadrac <rabbikindalo001@@gmale.com>
2023-08-23 13:38:29 +0200

    logim src

--- server.js
+++ server.js
@@ -48,12 +48,12 @@ app.get("/", (req, res) => {
     {
       Method: "GET",
       Urls: "/api/test/user",
-      Actions: "Access User’s content"
+      Actions: "Access User's content"
     },
     {
       Method: "GET",
       Urls: "/api/test/mod",
-      Actions: "Access Moderator’s content"
+      Actions: "Access Moderator's content"
     },
     {
       Method: "GET",
@@ -80,7 +80,7 @@ require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
 // set port, listen for requests       
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port there are the end points ${PORT}.`);
 });
